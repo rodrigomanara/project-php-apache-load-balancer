e28edfbbba4a78e66e43cd85bd5aa2b82e58141c
codediesel <rodrigomanara@yahoo.co.uk>
2025-02-17 10:48:18 +0000

    add regis, mysql elastic search

--- www/index.php
+++ www/index.php
@@ -1,4 +1,17 @@
 <?php
 
+$host = "db"; // e.g., "localhost" or "mysql" (if using Docker)
+$dbname = "db";
+$username = "root";
+$password = "root";
 
-phpinfo();
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+
+    echo "✅ Connected to MySQL successfully!";
+} catch (PDOException $e) {
+    die("❌ Connection failed: " . $e->getMessage());
+}
